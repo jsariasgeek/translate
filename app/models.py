d2b89ca47e5afb1b1995b8d2c7e5342460a98bf3
Johan Steven Arias V <johan@johan.(none)>
2014-09-11 21:52:23 -0500

    Meaning converted to 120 Characters

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Word(models.Model):
 	english = models.CharField(max_length=30)
-	spanish = models.CharField(max_length=30)
+	spanish = models.CharField(max_length=120)
 
 	def __unicode__(self):
 		return self.english
